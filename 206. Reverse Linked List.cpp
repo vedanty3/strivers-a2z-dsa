e2d3ca04dfe85a45af6f27b4880a9e2527d5d37f
vedant <ursvedantyetekar@gmail.com>
2023-01-09 06:56:02 +0530

    first four problems from linked list day 1

---
+++ 206. Reverse Linked List.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        ListNode *dummy = NULL;
+
+        while (head != NULL)
+        {
+            ListNode *head_nxt = head->next;
+            head->next = dummy;
+            dummy = head;
+            head = head_nxt;
+        }
+
+        return dummy;
+    }
+};
