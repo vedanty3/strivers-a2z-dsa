73e9353945fc4167478f0710350876b5ee7cced9
vedant <ursvedantyetekar@gmail.com>
2023-12-24 19:48:50 +0530

    set matrix zeroes

---
+++ 73. Set Matrix Zeroes.cpp
@@ -0,0 +1,93 @@
+// Approach 1
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    for (int k = 0; k < n; ++k) {
+                        if (matrix[i][k] == 0) {
+                            continue;
+                        }
+                        matrix[i][k] = -1;
+                    }
+                    for (int k = 0; k < m; ++k) {
+                        if (matrix[k][j] == 0) {
+                            continue;
+                        }
+                        matrix[k][j] = -1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+// Approach 2
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> dx(m, 1), dy(n, 1);
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 0) {
+                    dx[i] = 0;
+                    dy[j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (dx[i] == 0 or dy[j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+// Approach 3
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int col = 1;
+
+        for(int i=0; i<m; ++i){
+            if(matrix[i][0]==0) col = 0;
+            for(int j=1; j<n; ++j){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=m-1; i>=0; --i){
+            for(int j=n-1; j>=1; --j){
+                if(matrix[i][0]==0 or matrix[0][j]==0){
+                    matrix[i][j] = 0;
+                }
+            }
+            if(col==0) matrix[i][0] = 0;
+        }
+    }
+};
