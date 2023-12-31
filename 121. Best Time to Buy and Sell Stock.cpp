30e2d08fdd67fa52af2b808b45f3c785d0361a0c
vedant <uuvedant4@gmail.com>
2023-12-31 22:55:36 +0530

    Update 121. Best Time to Buy and Sell Stock.cpp

--- 121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -1,3 +1,22 @@
+// Approach 1
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+        
+        for(int i=0; i<prices.size(); ++i) {
+            for(int j=i+1; j<prices.size(); ++j) {
+                max_profit = max(max_profit, prices[j] - prices[i]);
+            }
+        }
+        
+        return max_profit;
+    }
+};
+
+// Approach 2
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
